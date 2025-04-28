221722aa2a942f89c65e707d42e1aac2b02ed335
ornobsingh <ornobsingh05@gmail.com>
2025-04-28 13:48:52 +0600

    adding load categories & send data to display

--- video.js
+++ video.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const loadCategories = async () => {
+  try {
+    const response = await fetch(
+      `https://openapi.programming-hero.com/api/phero-tube/categories`
+    );
+    const data = await response.json();
+    displayCategories(data.categories);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const displayCategories = (categories) => {
+  console.log(categories);
+};
+
+loadCategories();
