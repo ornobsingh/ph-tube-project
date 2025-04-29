1a8b326f145dd0a7db6f0aee0cf34f85da6507d0
ornobsingh <ornobsingh05@gmail.com>
2025-04-29 20:40:32 +0600

    Adding Load Videos And Display Cards On The Ui

---
+++ test.js
@@ -0,0 +1,18 @@
+/**{
+  "category_id": "1001",
+  "video_id": "aaag",
+  "thumbnail": "https://i.ibb.co/DRxB1Wm/sunris.jpg",
+  "title": "Sunrise Reverie",
+  "authors": [
+      {
+          "profile_picture": "https://i.ibb.co/yQFJ42h/ava.jpg",
+          "profile_name": "Ava Johnson",
+          "verified": false
+      }
+  ],
+  "others": {
+      "views": "1.1K",
+      "posted_date": "16950"
+  },
+  "description": "'Sunrise Reverie' by Ava Johnson takes listeners on a serene journey through tranquil melodies and soft harmonies. With 1.1K views, this track is perfect for morning relaxation or an evening wind-down. Ava's heartfelt lyrics and soothing voice create a sense of peace, making it a go-to for fans seeking calm and inspiration in their musical choices."
+}*/
--- video.js
+++ video.js
@@ -10,16 +10,56 @@ const loadCategories = async () => {
   }
 };
 
+const loadVideos = async () => {
+  try {
+    const response = await fetch(
+      `https://openapi.programming-hero.com/api/phero-tube/videos`
+    );
+    const data = await response.json();
+    displayVideos(data.videos);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const displayCategories = (categories) => {
-  const categoriesContainer = document.getElementById('categories');
+  const categoriesContainer = document.getElementById("categories");
 
   categories.forEach((item) => {
     const button = document.createElement("button");
     button.classList = "btn";
     button.innerText = item.category;
 
-    categoriesContainer.appendChild(button)
+    categoriesContainer.appendChild(button);
+  });
+};
+
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("videos");
+
+  videos.forEach((video) => {
+    console.log(video);
+    
+    const card = document.createElement("div");
+    card.innerHTML = 
+    `
+    <div class="card bg-base-100">
+  <figure>
+    <img
+      src= ${video.thumbnail} />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    `
+    videoContainer.appendChild(card)
   });
 };
 
 loadCategories();
+loadVideos();
